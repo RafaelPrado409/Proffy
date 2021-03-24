5d2e156bffc7347f7150d7cfad3a61297180c1c7
Rafael Prado <43122545+RafaelPrado409@users.noreply.github.com>
2021-03-24 02:51:23 -0300

    Add Mobile files
    Add Mobile files

---
+++ Mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { Image, Text, View, Linking } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+import api from '../../services/api';
+
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+  favorited: boolean;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+  const [isFavorited, setIsFavorited] = useState(favorited);
+
+  function handleLinkToWhatsapp() {
+    api.post('connections'),
+      {
+        user_id: teacher.id,
+      };
+
+    Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+  }
+
+  async function handleToggleFavorites() {
+    const favorites = await AsyncStorage.getItem('favorites');
+
+    let favoritesArray = [];
+
+    if (favorites) {
+      favoritesArray = JSON.parse(favorites);
+    }
+
+    if (isFavorited) {
+      const favoriteIndex = favoritesArray.findIndex((teacherItem: Teacher) => {
+        return teacherItem.id === teacher.id;
+      });
+      favoritesArray.splice(favoriteIndex, 1);
+
+      setIsFavorited(false);
+    } else {
+      favoritesArray.push(teacher);
+
+      setIsFavorited(true);
+    }
+    await AsyncStorage.setItem('favorites', JSON.stringify(favoritesArray));
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: teacher.avatar,
+          }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>{teacher.name}</Text>
+          <Text style={styles.subject}>{teacher.subject}</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>{teacher.bio}</Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
+        </Text>
+
+        <View style={styles.buttonContainer}>
+          <RectButton
+            onPress={handleToggleFavorites}
+            style={[styles.favoriteButton, isFavorited ? styles.favorited : {}]}
+          >
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+
+          <RectButton
+            onPress={handleLinkToWhatsapp}
+            style={styles.contactButton}
+          >
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default TeacherItem;
---
+++ Mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/core';
+
+import giveClassesImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
+
+function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesImage}
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar vocẽ precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+}
+
+export default GiveClasses;
